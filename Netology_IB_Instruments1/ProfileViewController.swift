dc2eb9e3911b5c642f1b513d1176f07da3d12c22
RONIN-79 <r.bagirli79@gmail.com>
2022-02-27 01:50:50 +0300

    third commit

--- Netology_IB_Instruments1/ProfileViewController.swift
+++ Netology_IB_Instruments1/ProfileViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profileView: ProfileView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
